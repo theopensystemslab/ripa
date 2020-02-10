86359d3c28763a70444ee9767d3e79372a9c91bf
houbly <h.blyden@gmail.com>
2020-02-10 16:22:02 +0000

    removed old theme ref

--- src/amira/Date.fixture.tsx
+++ src/amira/Date.fixture.tsx
@@ -1,12 +1,9 @@
 import { Button, TextField } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
-import { ThemeProvider } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import { useFormik } from "formik";
 import * as React from "react";
-
-import theme from "./defaultTheme";
 
 interface IText {
   title: string;
@@ -28,35 +25,33 @@ const Date: React.FC<IText> = ({ title, type, name, options, inputProps }) => {
     }
   });
   return (
-    <ThemeProvider theme={theme}>
-      <form onSubmit={formik.handleSubmit}>
-        <Typography variant="h2" component="div" gutterBottom>
-          {title}
-        </Typography>
-        <Grid container spacing={1}>
-          {options.map((el, index) => (
-            <Grid item key={`${el}-${index}`}>
-              <TextField
-                label={el}
-                required
-                onChange={formik.handleChange}
-                type={type}
-                name={`${name}-${el}`}
-                InputLabelProps={{
-                  shrink: true
-                }}
-                inputProps={inputProps[el]}
-              ></TextField>
-            </Grid>
-          ))}
-        </Grid>
-        <Box pt={3}>
-          <Button type="submit" variant="contained" color="primary">
-            Save and Continue
-          </Button>
-        </Box>
-      </form>
-    </ThemeProvider>
+    <form onSubmit={formik.handleSubmit}>
+      <Typography variant="h2" component="div" gutterBottom>
+        {title}
+      </Typography>
+      <Grid container spacing={1}>
+        {options.map((el, index) => (
+          <Grid item key={`${el}-${index}`}>
+            <TextField
+              label={el}
+              required
+              onChange={formik.handleChange}
+              type={type}
+              name={`${name}-${el}`}
+              InputLabelProps={{
+                shrink: true
+              }}
+              inputProps={inputProps[el]}
+            ></TextField>
+          </Grid>
+        ))}
+      </Grid>
+      <Box pt={3}>
+        <Button type="submit" variant="contained" color="primary">
+          Save and Continue
+        </Button>
+      </Box>
+    </form>
   );
 };
 export default {
