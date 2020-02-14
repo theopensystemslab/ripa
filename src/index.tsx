e0c28c514f2f7cb0cad6e6291cd8d9183efb8ce3
John Rees <john@bitsushi.com>
2020-02-14 12:04:56 +0000

    remove unnecessary content, set SignIn as homepage

--- src/index.tsx
+++ src/index.tsx
@@ -1,60 +1,16 @@
-import { ApolloProvider } from "@apollo/react-hooks";
-import ApolloClient from "apollo-boost";
-import { createBrowserNavigation } from "navi";
-import React, { Suspense, useEffect, useState } from "react";
+import { ThemeProvider } from "@material-ui/core";
+import React from "react";
 import ReactDOM from "react-dom";
-import { Router, View } from "react-navi";
-import HelmetProvider from "react-navi-helmet-async";
 
-import getCookie from "./lib/getCookie";
-import routes from "./routes";
+import defaultTheme from "./amira/themes/default";
+import SignIn from "./pages/SignIn";
 import * as serviceWorker from "./serviceWorker";
 
-const gqlClient = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_URL,
-  request: operation => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      operation.setContext({
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-    }
-  }
-});
-
-const navigation = createBrowserNavigation({
-  routes
-});
-
 const App = () => {
-  const [currentUser, setCurrentUser] = useState(false);
-
-  useEffect(() => {
-    const token = getCookie("jwt");
-    if (token) {
-      localStorage.setItem("token", token);
-      setCurrentUser(true);
-    } else {
-      localStorage.removeItem("token");
-      setCurrentUser(false);
-    }
-  }, [setCurrentUser]);
-
   return (
-    <ApolloProvider client={gqlClient}>
-      <HelmetProvider>
-        <Router
-          navigation={navigation}
-          context={{ navigation, gqlClient, currentUser }}
-        >
-          <Suspense fallback={<div>Loading... </div>}>
-            <View />
-          </Suspense>
-        </Router>
-      </HelmetProvider>
-    </ApolloProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <SignIn />
+    </ThemeProvider>
   );
 };
 
--- src/lib/log.ts
+++
@@ -1,4 +0,0 @@
-const log = (input, type = "info") =>
-  process.env.REACT_APP_DEBUG && console[type](input);
-
-export default log;
