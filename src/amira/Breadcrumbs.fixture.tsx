128be195b013d63aa4f5114a40c9cec06e68b39c
houbly <h.blyden@gmail.com>
2020-02-14 11:31:20 +0000

    Added Breadcrumbs

---
+++ src/amira/Breadcrumbs.fixture.tsx
@@ -0,0 +1,85 @@
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+import DividerIcon from "@material-ui/icons/ArrowForward";
+import classNames from "classnames";
+import * as React from "react";
+
+interface IMinMax {
+  min?: number;
+  max?: number;
+}
+interface IBreadcrumb {
+  active?: number;
+  list?: string[];
+}
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    padding: 0,
+    margin: 0,
+    alignItems: "center",
+    color: "currentColor",
+    flexWrap: "wrap",
+    "& li": {
+      color: "currentColor",
+      fontSize: "20px",
+      listStyle: "decimal inside",
+      whiteSpace: "nowrap",
+      paddingRight: "1rem",
+      fontWeight: "500"
+    }
+  },
+  breadcrumb: {
+    display: "flex",
+    alignItems: "center",
+    opacity: 0.75
+  },
+  active: {
+    fontWeight: 700,
+    opacity: 1
+  },
+  icon: {
+    color: "currentColor",
+    margin: "0 0.5em",
+    width: "0.75em",
+    height: "0.75em"
+  }
+});
+
+const Breadcrumb: React.FC<IBreadcrumb> = ({ active = 0, list = [] }) => {
+  const classes = useStyles();
+  return (
+    <Box bgcolor="background.paper" p={3}>
+      <Box fontSize="h6.fontSize" component="ol" className={classes.root}>
+        {list.map((child, i) => (
+          <div
+            className={classNames(
+              classes.breadcrumb,
+              i + 1 === active && classes.active
+            )}
+            key={i}
+          >
+            {i + 1}. {child}
+            {i + 1 < list.length && <DividerIcon className={classes.icon} />}
+          </div>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default {
+  default: (
+    <Breadcrumb
+      active={2}
+      list={[
+        "Property location",
+        "About the works",
+        "Application",
+        "Declaration",
+        "Pay fee"
+      ]}
+    />
+  )
+};
