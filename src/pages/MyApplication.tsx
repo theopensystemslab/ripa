5d6a2986d83aca9b609571626a77469e57988b63
John Rees <john@bitsushi.com>
2020-02-14 12:54:02 +0000

    add progress bar to MyApplication

--- src/pages/MyApplication.tsx
+++ src/pages/MyApplication.tsx
@@ -1,4 +1,17 @@
+import { withStyles } from "@material-ui/core";
+import LinearProgress from "@material-ui/core/LinearProgress";
 import * as React from "react";
+
+const BorderLinearProgress = withStyles({
+  root: {
+    height: 10,
+    backgroundColor: "#C5C5C5"
+  },
+  bar: {
+    borderRadius: 20,
+    backgroundColor: "#000000"
+  }
+})(LinearProgress);
 
 const MyApplication = ({ sections }) => {
   return (
@@ -6,6 +19,11 @@ const MyApplication = ({ sections }) => {
       <h1>My application</h1>
       <p>Please complete all sections</p>
       <p>12% complete</p>
+      <BorderLinearProgress
+        variant="determinate"
+        color="secondary"
+        value={12}
+      />
       <ul>
         {Object.entries(sections).map(([name, status]) => (
           <li key={name}>
