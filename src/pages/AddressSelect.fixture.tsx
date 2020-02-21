0cc1c9478a91d1f7010aabc390097a5e0b0ed4f8
John Rees <john@bitsushi.com>
2020-02-21 11:51:47 +0000

    fake addresses in cosmos

--- src/pages/AddressSelect.fixture.tsx
+++ src/pages/AddressSelect.fixture.tsx
@@ -2,4 +2,25 @@ import * as React from "react";
 
 import AddressSelect from "./AddressSelect";
 
-export default <AddressSelect />;
+export default (
+  <AddressSelect
+    addresses={[
+      {
+        id: 1,
+        name: "97 Grove Street"
+      },
+      {
+        id: 2,
+        name: "98 Grove Street"
+      },
+      {
+        id: 3,
+        name: "240A Carpenter St"
+      },
+      {
+        id: 4,
+        name: "240B Carpenter St"
+      }
+    ]}
+  />
+);
